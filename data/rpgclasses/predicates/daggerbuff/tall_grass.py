e2e17349b49a1674667a9698abe8b30033c7872e
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-08-03 14:12:48 +0200

    tabs

--- data/rpgclasses/predicates/daggerbuff/tall_grass.py
+++ data/rpgclasses/predicates/daggerbuff/tall_grass.py
@@ -2,8 +2,8 @@ import json
 
 
 d={
-    "condition": "minecraft:any_of",
-    "terms": []
+	"condition": "minecraft:any_of",
+	"terms": []
 }
 
 x_range = range(-2, 3)
@@ -11,21 +11,21 @@ y_range = range(-1, 2)
 z_range = range(-2, 3)
 
 for a in x_range:
-    for b in y_range:
-        for c in z_range:
-            d["terms"].append(
-                {
-                    "condition": "minecraft:location_check",
-                    "offsetX": a,
-                    "offsetY": b,
-                    "offsetZ": c,
-                    "predicate": {
-                        "block": {
-                            "tag": "rpgclasses:hidden_in_bushes"
-                        }
-                    }
-                }
-            )
+	for b in y_range:
+		for c in z_range:
+			d["terms"].append(
+				{
+					"condition": "minecraft:location_check",
+					"offsetX": a,
+					"offsetY": b,
+					"offsetZ": c,
+					"predicate": {
+						"block": {
+							"tag": "rpgclasses:hidden_in_bushes"
+						}
+					}
+				}
+			)
 
 
 print(json.dumps(d, indent=4))
